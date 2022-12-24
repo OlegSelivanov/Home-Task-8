2e7e6bea25fd2ee381654f2f42e7ae38ba31c3fe
Oleg Selivanov <alien3569@mail.ru>
2022-12-24 16:49:16 +0300

    Add fin

--- Task 54/Program.cs
+++ Task 54/Program.cs
@@ -17,7 +17,7 @@ void InputMatrix(int[,] matrix)
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
-            matrix[i, j] = new Random().Next(-10, 11); // [-10, 10]
+            matrix[i, j] = new Random().Next(1, 11); // [1, 10]
     }
 }
 
