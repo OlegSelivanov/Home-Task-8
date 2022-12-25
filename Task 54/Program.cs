d392ff113647ba4d176125e482eabb51d3d9fab4
Oleg Selivanov <alien3569@mail.ru>
2022-12-25 15:23:29 +0300

    Add fin

--- Task 54/Program.cs
+++ Task 54/Program.cs
@@ -11,24 +11,23 @@
 8 4 4 2
 */
 
-
 void InputMatrix(int[,] matrix)
 {
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-            matrix[i, j] = new Random().Next(1, 11); // [1, 10]
-    }
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+      matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+  }
 }
 
 void PrintMatrix(int[,] matrix)
 {
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-            Console.Write($"{matrix[i, j]} \t");
-        Console.WriteLine();
-    }
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+      Console.Write($"{matrix[i, j]} \t");
+    Console.WriteLine();
+  }
 }
 
 int[,] ReleaseMatrix(int[,] matrix)
@@ -55,8 +54,10 @@ Console.Clear();
 Console.Write("Введите размер матрицы: ");
 int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
 int[,] matrix = new int[size[0], size[0]];
+Console.WriteLine();
 Console.WriteLine("Начальный массив:");
 InputMatrix(matrix);
 PrintMatrix(matrix);
+Console.WriteLine();
 Console.WriteLine("Конечный массив:");
 PrintMatrix(ReleaseMatrix(matrix));
